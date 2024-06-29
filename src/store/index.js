39a71adb0cb32a7187fce538524bd59c2d7fd633
Sarmadmaqbool <sarmadhere@outlook.com>
2024-06-29 01:38:04 -0400

    set state management

---
+++ src/store/index.js
@@ -0,0 +1,19 @@
+import Vue from 'vue';
+import Vuex from 'vuex';
+
+Vue.use(Vuex);
+
+export default new Vuex.Store({
+  state: {
+    // Define your state here
+  },
+  mutations: {
+    // Define your mutations here
+  },
+  actions: {
+    // Define your actions here
+  },
+  modules: {
+    // Optionally, divide your store into modules
+  },
+});
